fffb495f0cecec5d4be8313e0dca178399e1306b
Essayas Abraham Hailu <80784659+tedy-abr@users.noreply.github.com>
2026-01-25 15:19:09 +0100

    Move API keys to environment variables

--- src/js/supabase.js
+++ src/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qmcgotpjyuswttkfngvw.supabase.co";
-const supabaseKey = "sb_publishable_rvXMm6OynXBsnO7pAl1Srg_0bgQNxEG";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
