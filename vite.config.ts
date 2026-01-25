dad7cb279fed5032850a9444fce52b60d1469769
Essayas Abraham Hailu <80784659+tedy-abr@users.noreply.github.com>
2026-01-25 04:38:15 +0100

    Configured vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        register: resolve(__dirname, "register.html"),
+        feed: resolve(__dirname, "feed.html"),
+        profile: resolve(__dirname, "profile.html"),
+        "create-post": resolve(__dirname, "create-post.html"),
+        "edit-post": resolve(__dirname, "edit-post.html"),
+      },
+    },
+  },
 });
